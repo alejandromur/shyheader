1da26972ee4bf29ee271339a66efb17c6c8cc188
amur <alejandro@mamutlove.es>
2016-03-04 10:59:08 +0100

    first commit

---
+++ js/jquery.shyheader.js
@@ -0,0 +1,110 @@
+/*
+ * BASED ON THE ORIGINAL IDEA OF Marius Craciunoiu
+ * https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c#.wc32ja29i
+ *
+ *
+ * jQuery.shyheader v0.1.0
+ * https://github.com/alejandromur/shyheader
+ * Copyright 2016, alejandro@mamutlove.es
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+(function( $, window, document, undefined ){
+
+    "use strict";
+
+    $.shyheader = function(el, options){
+
+        var base = this;
+
+        base.$el = $(el);
+        base.el = el;
+
+        base.$el.data('shyheader',base);
+
+        var IS_SCROLLING = false;
+        var SCROLL = 0;
+        var OLD_OFFSET = 0;
+        var CURRENT_OFFSET = 0;
+        var DELTA = 5;
+        var HEADER_HEIGHT = 0;
+        var BODY = "";
+
+        base.initialize = function(){
+            base.options = $.extend({},$.shyheader.defaultOptions, options);
+
+            HEADER_HEIGHT = base.$el.outerHeight(true);
+
+            if( base.options.container !== "undefined" ){
+                BODY = $('.'+base.options.container);
+                BODY.css("padding-top", HEADER_HEIGHT+"px");
+                base.options.offsetContentFlag = true;
+            }
+
+            window.addEventListener("scroll", base.triggerScroll, false);
+
+        };
+
+        base.triggerScroll = function(){
+            IS_SCROLLING = true;
+            SCROLL = document.body.scrollTop || window.pageYOffset;
+            base.checkScrollPosition();
+        };
+
+        base.checkScrollPosition = function(){
+            if( base.options.offsetContentFlag ){
+              if( SCROLL >= HEADER_HEIGHT ){
+                base.watch();
+              }
+            }else{
+                base.watch();
+            }
+        };
+
+        base.watch = function(){
+            if( IS_SCROLLING ) {
+                base.getDirection();
+                IS_SCROLLING = false;
+            }
+        };
+
+        base.getDirection = function() {
+
+            CURRENT_OFFSET = SCROLL;
+
+            if( Math.abs(OLD_OFFSET - CURRENT_OFFSET) <= DELTA ){
+                return;
+            }
+
+            if( CURRENT_OFFSET > OLD_OFFSET ){
+                base.$el.addClass(base.options.classname);
+            }else{
+                if( CURRENT_OFFSET + $(window).height() < $(document).height() ) {
+                    base.$el.removeClass(base.options.classname);
+                }
+            }
+
+            OLD_OFFSET = CURRENT_OFFSET;
+        };
+
+        base.initialize();
+
+    };
+
+
+    $.shyheader.defaultOptions = {
+        classname : "is-watching",
+        container : 'undefined',
+        offsetContentFlag : false
+    };
+
+    $.fn.shyheader = function(options){
+
+        return this.each(function(){
+            var shyheader = new $.shyheader(this,options);
+        });
+    };
+
+}( jQuery, window, document ));
---
+++ readme.md
@@ -0,0 +1,26 @@
+<h1>Shy Header</h1>
+<p>A simple, light-weight and easy-to-use jQuery Plugin that shows/hides the header of the page when users scroll the page down more than its height. However, the header remains watching the page so when users scroll the page up its being showed.</p>
+<p><a href="http://www.mamutlove.es/projects/shyheader/" title="Demo" target="_blank">Demo</a></p>
+<p>Please, take this <a href="http://www.mamutlove.es/projects/shyheader/" title="Demo" target="_blank">demo</a> as it is, a little demo where I pretend to show the power of this plugin, but consider your own target before copy/paste the code. It is possible that you need to reset some properties contained in the CSS file.</p>
+
+<h2>Customizable</h2>
+<p>• You could change the name of the class that is going to be added to the header ('is-watching' by default). Remember then to apply your styles to this new class.</p>
+<p>• Add an offset to your main container.<br>Due to the header is fixed to the top of the window, its height do not computable for the DOM, so the next sibling starts at the very top of the document. If you need to add an offset to this sibling equal as the header's height, set a class name 'shy-container' by default when initializing the plugin as a value for the option 'container'.</p>
+<p>• Customize or change the CSS files.</p>
+
+<h2>Usage</h2>
+<p>• Download or fork the repository.</p>
+<p>• Include jQuery and the Javascript file minify or not in your HTML.</p>
+<pre>
+    <b>// Initialize the plugin</b>
+    $("your-header-selector").shyheader();
+    <br>
+    <b>// some options are available</b>
+    $("your-header-selector").shyheader({
+        classname : "is-watching",
+        container : 'shy-container'
+    });
+</pre>
+
+<h2>Credits</h2>
+<p>• Feel free to download, modify, break, use or destroy this plugin.</p>"# shyheader" 
